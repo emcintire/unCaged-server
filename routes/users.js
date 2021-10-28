4a7f054f4e2ca4e0882f65c80460960375ad27b6
emcintire <everettgsm@gmail.com>
2021-10-27 23:16:29 -0400

    Fixed bug

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post('/', async (req, res) => {
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const user = await User.findOne({ email: req.body.email });
+    let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered');
 
     user = new User({
