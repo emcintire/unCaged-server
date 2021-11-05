8f05509b13947d5b68b8d3a4aae74a11d3a739b2
emcintire <everettgsm@gmail.com>
2021-11-04 23:26:21 -0400

    added privacy policy view

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(error);
 
+app.get('/privacy', function (request, response) {
+    response.sendFile(__dirname + '/public/privacy.html');
+});
+
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => console.log(`Connected to server`));
 
